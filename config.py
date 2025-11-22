651d54c284a873ef1437457fed65270b7c594f49
SrLuther <tioluther@gmail.com>
2025-11-21 23:50:06 -0300

    Enviando o arquivo config.py com as alterações de ID de salas de ticket

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # -------------------- CONFIGURAÇÕES DO BOT --------------------
 
 # --- Canais ---
-CANAL_PAINEL_ID = 1440918150957891656           # Painel de tickets
+CANAL_PAINEL_ID = 1440909767974453328           # Painel de tickets
 CANAL_ARQUIVO_ID = 1441236730517655634          # Canal para tickets arquivados
 CANAL_STATUS_ID = 1440828555201216582           # Canal de logs do bot (status, carregamento, comandos)
 TICKET_CATEGORY_ID = 1440909767974453328        # Categoria onde tickets serão criados
-TICKET_ARCHIVE_CHANNEL_ID = 144092111111111111  # Canal de arquivamento de tickets
+TICKET_ARCHIVE_CHANNEL_ID = 1441236730517655634  # Canal de arquivamento de tickets
 
 # --- Roles / Cargos ---
 MOD_ROLE_IDS = [1440828410556321882, 1440828412599210135]  # Cargos que podem acessar painel administrativo
