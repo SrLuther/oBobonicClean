4983fa1c06566967fdbee71f68509fc677038cb3
LUCIANO SANTOS COSTA <43154215+SrLuther@users.noreply.github.com>
2025-11-22 05:40:24 -0300

    Atualizar o config.py com adição de canal log e id do servidor

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 # -------------------- CONFIGURAÇÕES DO BOT --------------------
 
+# --- Identificadores Essenciais ---
+GUILD_ID = 1440802112601854159           # ID REAL DA SUA GUILDA/SERVIDOR!
+
 # --- Canais ---
 CANAL_PAINEL_ID = 1440909767974453328           # Painel de tickets
 CANAL_ARQUIVO_ID = 1441236730517655634          # Canal para tickets arquivados
-CANAL_STATUS_ID = 1440828427761487934           # Canal de boas vindas
+CANAL_STATUS_ID = 1440828427761487934           # Canal de boas vindas (Membros, Lista e Status Final)
 TICKET_CATEGORY_ID = 1441644856429772962        # Categoria onde tickets serão criados
 TICKET_ARCHIVE_CHANNEL_ID = 1441236730517655634  # Canal de arquivamento de tickets
+CANAL_LOGS_ID = 1440828555201216582             # Canal dedicado para Logs de Carregamento e Alertas!
 
 # --- Roles / Cargos ---
 MOD_ROLE_IDS = [1440828410556321882, 1440828412599210135]  # Cargos que podem acessar painel administrativo
@@ -14,3 +18,6 @@ STAFF_ROLE_ID = [1440828410556321882, 1440828412599210135] # Cargo usado nos tic
 # --- Configurações de Tickets ---
 EXPIRACAO_TICKET_HORAS = 48   # Tempo máximo de inatividade para fechar automaticamente tickets
 TICKET_ID_LENGTH = 5          # Tamanho do código de identificação do ticket
+
+# --- Configurações de Log ---
+LOG_SEPARATOR = "--------------------------------------------------------" 
